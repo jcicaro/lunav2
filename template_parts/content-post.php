2335a2a1cd5123644ccd0d250673fa386b00c0a6
jc.icaro <jc.icaro@gmail.com>
2019-04-18 05:00:39 -0400

    amend padding of date

--- template_parts/content-post.php
+++ template_parts/content-post.php
@@ -3,6 +3,12 @@
         <div class="card-custom-header" <?php echo (new Luna_Util())->fetch_post_background_image_style(); ?> >
             <div class="card-custom-img">
                 <h4 class="card-title"><?php the_title() ;?></h4>
+                
+                <span class="date">
+                    <span class="month"><?php the_time('M') ?></span>
+                    <span class="day"><?php the_time('j') ?></span>
+                    <span class="year"><?php the_time('Y') ?></span>
+                </span>
             </div>
         </div>
         
